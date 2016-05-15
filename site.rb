1dfa115e78d46e0a025f3facf68f729609e59eb8
Bryant Dresher <bryant.dresher@utah.edu>
2016-05-15 18:04:51 +0000

    Add read_more page

--- site.rb
+++ site.rb
@@ -1,20 +1,20 @@
 require 'bundler/setup'
 require 'sinatra'
 ENV['PORT'] ||= '4000'
- set :port, ENV['PORT']
+set :port, ENV['PORT']
 set :bind, '0.0.0.0'
- get '/' do
+get '/' do
    erb :index
  end
- get '/italian' do
+get '/italian' do
    "Ciao!"
  end
 get '/read_more' do
-   "When there's more to be read, you'll find it here…"
+   erb :read_more
  end
- get '/NewYork' do
+get '/NewYork' do
    "The City that never Sleeps!"
  end
- get '/Vinnie' do
+get '/Vinnie' do
    "He's a guy who knows guys!"
  end
