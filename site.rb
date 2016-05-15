4e6ca413b06e2437b50d8c9c7642ceaf6a8d670e
Bryant Dresher <bryant.dresher@utah.edu>
2016-05-15 07:42:46 +0000

    Heroku configs

--- site.rb
+++ site.rb
@@ -1,7 +1,8 @@
 require 'bundler/setup'
 require 'sinatra'
-set :port, 4000
- set :bind, '0.0.0.0'
+ENV['PORT'] ||= '4000'
+ set :port, ENV['PORT']
+set :bind, '0.0.0.0'
  get '/' do
    erb :index
  end
